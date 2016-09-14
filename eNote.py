ea6b48c1047916bb498944af6e636b7f4050c12e
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-09-14 10:19:40 +0300

    Add some history

--- eNote.py
+++ eNote.py
@@ -13,25 +13,42 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 # Global block
-chat_history = 'test.tmp'
+chat_history = 'chat_history.txt'
+today = datetime.now()
+today = today.strftime("%Y-%m-%d")
+yesterday = datetime.now() - timedelta(days=1)
+yesterday = yesterday.strftime("%Y-%m-%d")
 
 def xyu(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id, text="Пизда!")	
 
 def record(bot, update):
     f = open(chat_history, 'w')
+
     message = update.message.text
-	message = message.split(' ', 1)[1]	
-	f.write(message)
+	message = message.split(' ', 1)[1]
 
-	f.close()
+    data = {
+        date: today,
+        text: message
+    }
 
-def digest(bot, update):
-	f = open(chat_history, 'r')
+    # Convert to JSON & append to the file
+    f = open(chat_history, 'a+')
+    f.write(json.dumps(data) + '\n')
+    f.close()
 
-#	if args == False:
-	bot.sendMessage(chat_id=update.message.chat_id, text=f.readline())
-	f.close()
+
+def digest(bot, update, args):
+    with open(chat_history) as f:
+        for line in f:
+            data = json.loads(line)
+            if args[0] == 'сегодня' or args == False:
+                if data['date'] == today:
+                    bot.sendMessage(chat_id=update.message.chat_id, text=data['text'])
+            elifif args[0] == 'вчера':
+                if data['date'] == yesterday:
+                    bot.sendMessage(chat_id=update.message.chat_id, text=data['text'])
 
 updater = Updater(token='283098184:AAEztJC92M9wczX0WyXd1vuHuF7uM3ObeuU')
 dispatcher = updater.dispatcher
